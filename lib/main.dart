886de9eb2b10de3a49e4c898596126f68816eae2
Lucas Silva Silvestre <lucas_silvestre.10@hotmail.com>
2021-08-21 19:41:03 -0300

    Adicionado função de de toque do instrumento

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(MarimbaApp());
+
+void tocarSom(int numeroSom) {
+  final player = AudioCache();
+  player.play('nota$numeroSom.wav');
+}
+
+void criarBotao() {
+  Expanded(
+    child: TextButton(
+        onPressed: () {
+          tocarSom(1);
+        },
+        style: TextButton.styleFrom(backgroundColor: Colors.red),
+        child: null),
+  );
+}
 
 class MarimbaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[],
+            ),
+          ),
         ),
       ),
     );
